12f66cbfca0dd6e8193d819181cee9994488be15
Jarrod Marshall <jarrod@jarrodmarshall.io>
2023-03-28 13:24:43 -0500

    Initial commit

---
+++ ShakesClient/Program.cs
@@ -0,0 +1,32 @@
+﻿ // See https://aka.ms/new-console-template for more information
+
+ using System;
+ using Microsoft.Extensions.DependencyInjection;
+ 
+
+ namespace ShakesClient
+ {
+     class Program
+     {
+         static async Task Main(string[] args)
+         {
+             var serviceCollection = new ServiceCollection();
+
+             serviceCollection
+                 .AddConferenceClient()
+                 .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+             IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+             IConferenceClient client = services.GetRequiredService<IConferenceClient>();
+
+             var result = await client.GetSessions.ExecuteAsync();
+             result.EnsureNoErrors();
+
+             foreach (var session in result.Data?.Sessions.Nodes)
+             {
+                 Console.WriteLine(session.Title);
+             }
+         }
+     }
+ }
